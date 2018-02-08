5c60ae39b1e023008323b587c3f8331bfb2ab9cc
p-nowicki <piotradam.nowicki@gmail.com>
2018-02-08 20:39:29 +0100

    A Hello World program.

--- src/main/java/pl/coderstrust/udemy/HelloWorld.java
+++ src/main/java/pl/coderstrust/udemy/HelloWorld.java
@@ -1,8 +1,7 @@
-package pl.coderstrust.udemy.section3;
+package pl.coderstrust.udemy;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
 }

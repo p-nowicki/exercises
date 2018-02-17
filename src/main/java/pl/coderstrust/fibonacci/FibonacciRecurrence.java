ada87368632d0521c4cdd345919fe97e0f66498e
p-nowicki <piotradam.nowicki@gmail.com>
2018-02-17 11:57:14 +0100

    my first commit

---
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecurrence.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+package pl.coderstrust.fibonacci;
+
+public class FibonacciRecurrence {
+
+    public static void main(String[] args) {
+        int a = 1; int b = 1; int temp;
+
+        System.out.print("Którą liczbę Fibonacciego wyznaczyć? ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int i = 0; i < n; ++i) {
+            temp = a;
+            a = b;
+            b += temp;
+        }
+
+        System.out.println(Integer.toString(n) + ". liczba Fibonacciego to " +
+                Integer.toString(a));
+    }
+}

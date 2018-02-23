f53e5eaa9607832ad4c4a8305b193c0ad568edf6
p-nowicki <piotradam.nowicki@gmail.com>
2018-02-23 16:57:18 +0100

    Fibonacci + tests

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -1,19 +1,14 @@
 package pl.coderstrust.fibonacci;
 
-import java.util.Scanner;
-
 public class FibonacciIterative {
 
-    private static long Iteration() {
+    public static long iteration(int count) {
 
-        long n1 = 0, n2 = 1, n3, i, count;
-        Scanner scanner = new Scanner(System.in);
-        count = scanner.nextInt();
-        System.out.print(n1 + " " + n2);
-
-        for (i = 2; i < count; ++i) {
+        long n1 = 0;
+        long n2 = 1;
+        long n3;
+        for (int i = 0; i < count; ++i) {
             n3 = n1 + n2;
-            System.out.print(" " + n3);
             n1 = n2;
             n2 = n3;
         }
@@ -22,7 +17,7 @@ public class FibonacciIterative {
 
     public static void main(String[] args) {
 
-        Iteration();
+        iteration(1);
 
     }
 }
--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -1,26 +1,22 @@
 package pl.coderstrust.fibonacci;
 
-import java.util.Scanner;
-
 public class FibonacciRecursive {
 
-    private static long Recursion(int n) {
+    public static long recursion(int n) {
 
         if (n < 0) {
             return 1;
         } else if (n > 0) {
-            return Recursion(n - 1) + Recursion(n - 2);
+            return recursion(n - 1) + recursion(n - 2);
         }
         return 0;
 
     }
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int numbers = sc.nextInt();
+        int numbers = 10;
         for (int i = 0; i <= numbers; ++i) {
-            long result = Recursion(i);
+            long result = recursion(i);
             System.out.print(result + " ");
         }
 

31ef6a3dfeb0ed84a4c7fba492e49d77cead4173
p-nowicki <piotradam.nowicki@gmail.com>
2018-02-25 18:12:25 +0100

    Pascal - tests to be done

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -1,15 +1,32 @@
 package pl.coderstrust.pascal;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PascalTriangle
 {
     public static void main(String args[])
     {
-        PascalPrint();
+        pascalPrint();
     }
 
-    private static void PascalPrint() {
+    public List<String> pascalTriangle;
+    public PascalTriangle() {
+        pascalTriangle = new ArrayList<String>();
+        pascalTriangle = new ArrayList<String>();
+        pascalTriangle.add("     1 ");
+        pascalTriangle.add("    1 1 ");
+        pascalTriangle.add("   1 2 1 ");
+        pascalTriangle.add("    1 3 3 1 ");
+        pascalTriangle.add("  1 4 6 4 1 ");
+    }
+
+    public List<String> getPascalTriangle() {
+        return pascalTriangle;
+    }
+
+    public static void pascalPrint() {
         int r, i, k, number, j;
         Scanner sc = new Scanner(System.in);
 
---
+++ src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
@@ -0,0 +1,9 @@
+package pl.coderstrust.pascal;
+
+import static org.junit.Assert.*;
+
+public class PascalTriangleTest {
+
+
+
+}
